7c105a39a1ab0eeed4ff813dffab2a5c6c14f5ac
oskarbraten <oskar.braten@outlook.com>
2019-06-23 16:58:14 +0200

    Updated deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
+
+TARGET=pi@sprinkler.local
+
+echo "Building."
 cargo build --release --target=armv7-unknown-linux-gnueabihf
-scp ./target/armv7-unknown-linux-gnueabihf/release/sprinkler pi@sprinkler.local:~/sprinkler
+
+# Setting up master connection (SSH).
+SSH_SOCKET=~/.ssh/$TARGET
+echo "Opening connection..."
+ssh -M -f -N -o ControlPath=$SSH_SOCKET $TARGET
+
+# Stopping service:
+echo "Stopping sprinkler service."
+ssh -q -o ControlPath=$SSH_SOCKET -t -p 22 $TARGET "sudo systemctl stop sprinkler"
+
+# Transfer executable using SCP.
+echo "Deploying executable."
+scp -o ControlPath=$SSH_SOCKET ./target/armv7-unknown-linux-gnueabihf/release/sprinkler $TARGET:~/sprinkler/sprinkler
+
+# Restarting service:
+echo "Starting sprinkler service."
+ssh -q -o ControlPath=$SSH_SOCKET -t -p 22 $TARGET "sudo systemctl start sprinkler"
+
+# Done.
+echo "Closing connection."
+
+ssh -S $SSH_SOCKET -O exit $TARGET
+echo "Deployment complete."
