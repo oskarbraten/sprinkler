c3b314c34ccfada0856b61b0bd97c4f819d3daba
oskarbraten <oskar.braten@outlook.com>
2019-06-25 23:12:32 +0200

    Fixed configuration sync issues and simplified code.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,10 @@
+use std::io;
+use std::fs::File;
+use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
 
-use super::time::{Moment, Interval};
 use super::schedule::Schedule;
+use super::time::{Interval, Moment};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Configuration {
@@ -19,8 +22,44 @@ impl Configuration {
                     Interval::new(Moment::new(16, 00, 00), Moment::new(16, 00, 05)),
                     Interval::new(Moment::new(16, 01, 00), Moment::new(16, 01, 10)),
                     Interval::new(Moment::new(20, 40, 00), Moment::new(20, 40, 10)),
-                ]
+                ],
+            },
+        }
+    }
+
+    pub fn load_from(path: &str) -> Self {
+        let mut file = match File::open(path) {
+            Ok(file) => file,
+            _ => {
+                println!("No configuration file. Creating with defaults.");
+
+                let mut file =
+                    File::create(path).expect("Unable to create configuration file (config.json).");
+                file.write_all(
+                    serde_json::to_string_pretty(&Configuration::default())
+                        .unwrap()
+                        .as_bytes(),
+                )
+                .expect("Failed to write configuration file.");
+
+                File::open("./config.json").expect("Unable to read config file.")
             }
+        };
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)
+            .expect("Unable to read configuration file (config.json).");
+
+        match serde_json::from_str(&contents) {
+            Ok(c) => c,
+            Err(msg) => panic!("Failed to parse configuration file (config.json), make sure it is correctly formatted. Error: {}", msg)
+        }
+    }
+
+    pub fn save_to(&self, path: &str) -> Result<(), io::Error> {
+        match File::create(path) {
+            Ok(mut file) => file.write_all(serde_json::to_string_pretty(self).unwrap().as_bytes()),
+            Err(e) => Err(e)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 
 use std::io;
-use std::io::prelude::*;
-use std::fs::File;
 use std::thread;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc};
 
 use actix_web::{HttpServer, App, web, middleware, HttpRequest};
 use actix_web::web::{Data, Json};
@@ -24,57 +22,31 @@ const ADDRESS: &str = "0.0.0.0:80";
 
 const TICKRATE: u64 = 1000;
 
-fn get_config(_req: HttpRequest, data: Data<Arc<Mutex<Configuration>>>) -> Json<Configuration> {
-    Json(data.lock().unwrap().clone())
+fn get_config(_req: HttpRequest) -> io::Result<fs::NamedFile> {
+    fs::NamedFile::open("config.json")
 }
 
-fn put_config(body: Json<Configuration>, data: Data<Arc<Mutex<Configuration>>>, dtx: Data<mpsc::Sender<Configuration>>) -> Json<Configuration> {
-    let mut config = data.lock().unwrap();
-    *config = body.into_inner(); // overwrite existing configuration.
-
-    dtx.send(config.clone()).unwrap();
-
-    // Save updated configuration:
-    match File::create("./config.json") {
-        Ok(mut file) => {
-            file.write_all(serde_json::to_string_pretty(&config.clone()).unwrap().as_bytes()).expect("Failed to write configuration file.");
+fn put_config(mut body: Json<Configuration>, dtx: Data<mpsc::Sender<Configuration>>) -> Json<Configuration> {
+    body.schedule.sort(); // sort schedule before updating.
+    match body.save_to("./config.json") {
+        Err(_) => {
+            panic!("Unable to save to configuration file.");
         },
-        _ => {
-            println!("Unable to open configuration file.");
-        }
+        _ => {}
     };
 
-    Json(config.clone()) // send back the updated configuration.
+    dtx.send(body.clone()).unwrap();
+    Json(body.clone()) // send back the updated configuration.
 }
 
 fn main() -> io::Result<()> {
-    let mut file = match File::open("./config.json") {
-        Ok(file) => file,
-        _ => {
-            let mut file = File::create("./config.json").expect("Unable to create configuration file (config.json).");
-            file.write_all(serde_json::to_string_pretty(&Configuration::default()).unwrap().as_bytes()).expect("Failed to write configuration file.");
-            
-            File::open("./config.json").expect("Unable to read config file.")
-        }
-    };
-
-    let config: Configuration = {
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).expect("Unable to read configuration file (config.json).");
-
-        match serde_json::from_str(&contents) {
-            Ok(c) => c,
-            Err(msg) => panic!("Failed to parse configuration file (config.json), make sure it is correctly formatted. Error: {}", msg)
-        }
-    };
-    
     // Setup mpsc-channel.
     let (tx, rx) = mpsc::channel::<Configuration>();
 
     // Start irrigation system:
     {
-        let c = config.clone();
-        thread::spawn(move || schedule::scheduler(c, rx, TICKRATE));
+        let config = Configuration::load_from("./config.json");
+        thread::spawn(move || schedule::scheduler(config, rx, TICKRATE));
     }
 
     // Start webserver:
@@ -88,7 +60,7 @@ fn main() -> io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .data(Arc::new(Mutex::new(config.clone())))
+            //.data(Arc::new(Mutex::new(config.clone())))
             .data(tx.clone())
             .wrap(middleware::Logger::default())
             .service(
